ce8e720ae99f0b5c66ee57b11d791b24cb82c91b
tdwlop <thisisvsemail@gmail.com>
2021-12-10 22:45:09 -0800

    updating home

--- Protodes/JS/PageFunctions.js
+++ Protodes/JS/PageFunctions.js
@@ -4,7 +4,33 @@ function viewAllHousing(){
     let mdiv = document.getElementById('vahold');
     for(let i = 1; i < 25; i++){
         mdiv.innerHTML = mdiv.innerHTML + 
-        '<div class="row" style=" margin: 50px 5px 5px 5px; border-radius: 10px; padding: 5px;" ><div class="col-md-4 bg-image shadow-1-strong"style="background-image: url(../Images/CSC642photo/joel-filipe-RFDP7_80v5A-unsplash.jpg);background-size: cover;height: calc(0.2 * 100vw); left:0; margin-right: 20px;"></div><div class="col"><div><h3 style="font-weight: bold;">Address</h3></div></br><div><h5>Special Tags</h5><div><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"><label class="form-check-label" for="defaultCheck1">Single</label></div><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"><label class="form-check-label" for="defaultCheck1">Pet Friendly</label></div><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"><label class="form-check-label" for="defaultCheck1">Party Friendly</label></div></div></div></div><div class="col"><h1>5</h1><div style="border: 2px solid black;border-radius: 10px; position:absolute; bottom: 0; padding-top: 10px; padding-left: 10px; padding-bottom: 10px; padding-right: 40px;"><p style="font-weight: bold;">Rent</p><h5 style="font-weight: bold; font-size: large;">$ 835.75/mo</h5></div></div></div>'
+        `<div class="row" style=" margin: 50px 5px 5px 5px; border-radius: 10px; padding: 5px;" >
+        <div class="col-md-4 bg-image shadow-1-strong"style="background-image: url(../Images/CSC642photo/joel-filipe-RFDP7_80v5A-unsplash.jpg);background-size: cover;height: calc(0.2 * 100vw); left:0; margin-right: 20px;">
+        </div>
+        <div class="col">
+        <div>
+        <h3 style="font-weight: bold;">Address</h3>
+        </div>
+        </br>
+        <div>
+        <h5>Special Tags</h5>
+        <div>
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">Single</label>
+        </div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">Pet Friendly</label>
+        </div>
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">Party Friendly</label>
+        </div></div></div></div>
+        <div class="col" style="padding:calc(.2*10vw);">
+        <img src="../Images/CSC642photo/humanicon.png" style="height: calc(.2*30vw);float:left"/>
+        <h1>5</h1><div style="border: 2px solid black;border-radius: 10px; position:absolute; bottom: 0; padding-top: 10px; padding-left: 10px; padding-bottom: 10px; padding-right: 40px;">
+        <p style="font-weight: bold;">
+        Rent</p><h5 style="font-weight: bold; font-size: large;">$ 835.75/mo</h5></div></div></div>`
     }
 }
 
@@ -27,10 +53,24 @@ function login(pagename){
     `<div class="cotainer" id="loginp" style="display: table;position: absolute;top: 0;left: 0;height: 100%;width: 100%;z-index: 1000;"><div class="row justify-content-center" style="display: table-cell;vertical-align: middle;"><div class="col-md-4" style="border: 5px solid black; border-radius: 10px; box-shadow: 5px; margin-left: auto;margin-right: auto; background-color: white; border: 5px solid black; 
     border-radius: 10px; 
     padding: 20px 30px ;
-    " id="loginb"><a onclick="cancelled()"><span class="material-icons"style="top:0; right:0; position: absolute;">cancel</span><div class="row"></a><div class="col align-self-center" style="text-align: center;"><img class="img-thumbnail" style="border: none; " src="../Images/gator_web 1.png"></div></div><form action="/action_page.php"><p style="font-weight: bold; font-size: 20px;">Log in</p><div class="form-group"><input type="email" class="form-control" style="border: 2px solid black; border-radius: 10px;" id="email"placeholder="Username"><div style="text-align: right;"> <a href="#" style="align-self: right; font-size: 15px;">ForgotUsername?</a></div></div><div class="form-group"><input type="password" class="form-control" style="border: 2px solid black;border-radius: 10px;" id="pwd"placeholder="Password"><div style="text-align: right;"> <a href="#" style="align-self: right;font-size: 15px;">Forgot Password?</a></div></div><div style="text-align: center;"><button type="button" onclick="logged('${npn}')" class="btn-sm" id="loginbut" style="background-color: #34328F;
+    " id="loginb"><a onclick="cancelled()">
+    <span class="material-icons"style="top:0; right:0; position: absolute;">cancel
+    </span><div class="row"></a><div class="col align-self-center" style="text-align: center;">
+    <img class="img-thumbnail" style="border: none; " src="../Images/gator_web 1.png"></div>
+    </div><form action="/action_page.php"><p style="font-weight: bold; font-size: 20px;">Log in</p>
+    <div class="form-group">
+    <input type="email" class="form-control" style="border: 2px solid black; border-radius: 10px;" id="email"placeholder="Username"><div style="text-align: right;"> 
+    <a href="#" style="align-self: right; font-size: 15px;">ForgotUsername?</a></div></div>
+    <div class="form-group"><input type="password" class="form-control" style="border: 2px solid black;border-radius: 10px;" id="pwd"placeholder="Password"><div style="text-align: right;"> <a href="#" style="align-self: right;font-size: 15px;">Forgot Password?</a>
+    </div></div><div style="text-align: center;"><button type="button" onclick="logged('${npn}')" class="btn-sm" id="loginbut" style="background-color: #34328F;
     color: white;
     border: 2px solid #34328F;
-    border-radius: 50px;height:40px; padding: 0;"><p style="padding:0; margin:0;  font-size: 20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Log in&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></button></div> <div class="row justify-content-around"><div class="col-md-4"><div style="border:20x solid black; text-align: right;"><p style="font-weight:900;">&nbsp;&nbsp;_______________&nbsp;&nbsp;</p></div></div><div class="col-md-2" style="text-align: center; margin-top: 10px;"><p style="font-weight: bold;">OR</p></div><div class="col-md-4"><div style="border:20x solid black ; text-align: left;"><p style="font-weight:900;">_______________&nbsp;&nbsp;</p></div></div></div><a style="text-decoration: none" onclick="need2Reg()"><div style="text-align: center;"><button type="button" class="btn-sm" id="regbut" style="background-color: white;
+    border-radius: 50px;height:40px; padding: 0;"><p style="padding:0; margin:0;  font-size: 20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Log in&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></button></div> <div class="row justify-content-around">
+    <div class="col-md-4"><div style="border:20x solid black; text-align: right;">
+    <p style="font-weight:900;">&nbsp;&nbsp;_______________&nbsp;&nbsp;</p></div></div><div class="col-md-2" style="text-align: center; margin-top: 10px;"><p style="font-weight: bold;">OR</p></div><div class="col-md-4">
+    <div style="border:20x solid black ; text-align: left;">
+    <p style="font-weight:900;">_______________&nbsp;&nbsp;</p></div></div></div><a style="text-decoration: none" onclick="need2Reg()">
+    <div style="text-align: center;"><button type="button" class="btn-sm" id="regbut" style="background-color: white;
     color:#34328F;
     border:2px solid #34328F;
     border-radius: 50px;height:40px; padding:0;"><p style="padding:0; margin:0; font-size: 20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Register&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></button></div></a></form></div></div></div>`;
@@ -45,7 +85,10 @@ function logged(pagename){
         window.location = "../Pablo's Work/CreateEvent.html";
     }else if(pagename === 'Books'){
         window.location = '../HTML/BooksForm.html';
-    }else{
+    }else if(pagename === 'home'){
+        window.location = "../HTML/HomePage.html";
+    }
+    else{
         console.log('Pagename error');
     }
 }
@@ -103,7 +146,9 @@ function need2Reg(){
     lg.innerHTML = `<div class="row justify-content-center" style="display: table-cell;vertical-align: middle;"><div class="col-md-4" style="border: 5px solid black; border-radius: 10px; box-shadow: 5px; margin-left: auto;margin-right: auto; background-color: white;border: 5px solid black; 
     border-radius: 10px; 
     padding: 20px 30px ;
-    " id="loginb"><a onclick="cancelled()"><span class="material-icons"style="top:0; right:0; position: absolute;">cancel</span></a><div class="row"><div class="col align-self-center" style="text-align: center;"><img class="img-thumbnail" style="border: none; " src="../Images/gator_web 1.png"></div></div><form action="/action_page.php"><p style="font-weight: bold; font-size: 20px;">Register</p><div class="form-group"><input type="email" class="form-control" style="border: 2px solid black; border-radius: 10px;" id="email"placeholder="SFSU email"></div><div class="form-group"><input type="email" class="form-control" style="border: 2px solid black; border-radius: 10px;" id="email"placeholder="Username"></div><div class="form-group"><input type="password" class="form-control" style="border: 2px solid black;border-radius: 10px;" id="pwd"placeholder="Password"></div><div style="text-align: center;"><button type="button" class="btn-sm" id="loginbut" style=" background-color: #34328F;
+    " id="loginb"><a onclick="cancelled()"><span class="material-icons"style="top:0; right:0; position: absolute;">cancel</span></a><div class="row"><div class="col align-self-center" style="text-align: center;"><img class="img-thumbnail" style="border: none; " src="../Images/gator_web 1.png"></div></div><form action="/action_page.php"><p style="font-weight: bold; font-size: 20px;">Register</p><div class="form-group">
+    <input type="email" class="form-control" style="border: 2px solid black; border-radius: 10px;" id="email"placeholder="SFSU email"></div><div class="form-group"><input type="email" class="form-control" style="border: 2px solid black; border-radius: 10px;" id="email"placeholder="Username"></div><div class="form-group"><input type="password" class="form-control" style="border: 2px solid black;border-radius: 10px;" id="pwd"placeholder="Password"></div>
+    <div style="text-align: center;"><button type="button" class="btn-sm" id="loginbut" style=" background-color: #34328F;
     color: white;
     border: 2px solid #34328F;
     border-radius: 50px;height:40px; padding:0;"><p style="padding:0; margin:0; font-size: 20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Register&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></button></div></form></div></div>`
@@ -124,4 +169,9 @@ function cancelled(){
     bg.style.content = 'normal';
     bg.style.pointerEvents = 'auto';
     bg.style.zIndex = 10;
+}
+
+//to redirect to the view when user clicks on a card
+function vcardhome(picurl){
+    
 }
